d975d3222d11407af4a3d4d5a0e913e1896effcd
Guimarães Mahota <gmahota@accsys.co.mz>
2015-11-02 23:14:59 +0200

    acerto javascrit das tabelas selecionas

---
+++ MIT.CRM/src/MIT.CRM/Controllers/CobrancasController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MIT.CRM.Controllers
+{
+    public class CobrancasController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult EnviaEmail(string client)
+        {
+            
+            return Json(new
+            {
+                Success = true,
+                Message = "The person has been added!"
+                //PartialViewHtml = RenderPartialViewToString("PersonList", new PersonListViewModel { PersonList = _personList })
+            });
+
+        }
+
+    }
+}
